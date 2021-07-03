c828cd27937d6ee38a596190da6166cebbbff4c2
shell <shellipov@mail.ru>
2021-07-03 19:06:43 +0300

    interceptor

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+// let config = {
+//   headers: {
+//     'Permissions-Policy': 'interest-cohort=()',
+//   }
+// }
+
 const $host = axios.create({
     baseURL: process.env.REACT_APP_API_URL
 })
@@ -10,10 +16,17 @@ const $authHost = axios.create({
 
 const authInterceptor = config => {
     config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
+    config.headers['Permissions-Policy'] = 'interest-cohort=()'
+    return config
+}
+
+const interceptor = config => {
+    config.headers['Permissions-Policy'] = 'interest-cohort=()'
     return config
 }
 
 $authHost.interceptors.request.use(authInterceptor)
+$host.interceptors.request.use(interceptor)
 
 export {
     $host,
